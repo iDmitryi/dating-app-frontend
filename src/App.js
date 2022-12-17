3e5e6fa73e24e18ae81925803d65f46613f1521a
iDmitryi <hdumitru1998@gmail.com>
2022-12-17 15:18:10 +0200

    created DatingCards component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import DatingCards from "./components/DatingCards";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <DatingCards />
     </div>
   );
 }
